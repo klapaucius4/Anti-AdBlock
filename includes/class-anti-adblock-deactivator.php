6d7eb7a5e3a1adef6c694bc56ef9e0e904fc643b
klapaucius4 <klapaucius.mk@gmail.com>
2020-01-03 12:37:07 +0100

    first commit

---
+++ includes/class-anti-adblock-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://fsylum.net
+ * @since      1.0.0
+ *
+ * @package    Anti_AdBlock
+ * @subpackage Anti_AdBlock/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Anti_AdBlock
+ * @subpackage Anti_AdBlock/includes
+ * @author     Firdaus Zahari <firdaus@fsylum.net>
+ */
+class Anti_AdBlock_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
