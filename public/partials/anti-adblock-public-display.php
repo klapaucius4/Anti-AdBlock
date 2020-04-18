88f479d50be495ce5e17ceea400e3f293e72c912
klapaucius4 <klapaucius.mk@gmail.com>
2020-04-19 00:28:48 +0200

    mods

--- public/partials/anti-adblock-public-display.php
+++ public/partials/anti-adblock-public-display.php
@@ -8,7 +8,14 @@
     <div id="adbp-tab-1" class="flex-section">
         <div class="adbp-text-section">
             <h2>We detected <strong>Ad blocker</strong> extension!</h2>
-            <p>Your browser using extension to disable advertising on our website. Our page </p>
+            <p>Your browser using extension to disable advertising on our website. Please disable it, if you use one of the following or other ad-blocker extension.</p>
+            <div class="adbp-flexbox w-100">
+              <?php foreach(EXTENSION_LIST as $ext): ?>
+              <div class="w-15">
+                <?php require plugin_dir_path( __FILE__ ) . '../img/icons/'.$ext.'.svg'; ?>
+              </div>
+              <?php endforeach; ?>
+            </div>
         </div>
         <div class="image-section">
             <img class="adbp-image" src="<?= $this->plugin_location; ?>public/img/sad-man.gif" alt="" />
@@ -19,17 +26,28 @@
             <h2>How to disable adblock in <strong><?= $this->browser->getBrowser(); ?></strong></h2>
         </div>
         <div class="adbp-flexbox">
-            <?php foreach(EXTENSION_LIST as $icon): ?>
+            <?php foreach(EXTENSION_LIST as $ext): ?>
             <div class="adbp-flex-item">
-              <div class="adbp-extension-icon">
-                <?php require plugin_dir_path( __FILE__ ) . '../img/icons/'.$icon.'.svg'; ?>
+              <?php /*
+              <div class="w-25">
+                <?php require plugin_dir_path( __FILE__ ) . '../img/icons/'.$ext.'.svg'; ?>
+              </div>
+              */ ?>
+              <div class="extension-icon">
+                <?php require plugin_dir_path( __FILE__ ) . '../img/icons/'.$ext.'.svg'; ?>
+              </div>
+              <div class="w-100">
+                <video autoplay loop>
+                  <source src="<?= $this->plugin_location; ?>public/movies/<?= $ext; ?>.mp4" type="video/mp4">
+                  <!-- <source src="movie.ogg" type="video/ogg"> -->
+                </video>
               </div>
             </div>
             <?php endforeach; ?>
           </div>
         <div class="bg-container"><?php require_once plugin_dir_path( __FILE__ ) . '../img/icons/'.$this->browser_slug.'.svg'; ?></div>
     </div>
-    <div class="flex-section center">
+    <div class="flex-section center mt-20">
       <button id="adbp-how-to-disable-button" class="button button3">How to disable ad blocker plugin in <?= $this->browser->getBrowser(); ?>?</button>
       <button id="adbp-reload-page-button" class="button button2">I disabled ad blocker extension. Reload page.</button>
     </div>
